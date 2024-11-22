8b89da1061871deebef12e6c248bd338ea1f4d15
v_amerk <devamerkh@gmail.com>
2024-11-22 21:36:38 +0500

    add event filters

--- debugDivas/events/views.py
+++ debugDivas/events/views.py
@@ -1,7 +1,14 @@
 from rest_framework import viewsets
 from .models import Event
 from .serializers import EventSerializer
+from .filters import EventFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    filterset_class = EventFilter
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ['name']
+    ordering_fields = ['started_at', 'ended_at']
