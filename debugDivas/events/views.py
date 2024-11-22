cc859de4f57cf35d36ad66dbcada79befd37c1d6
v_amerk <devamerkh@gmail.com>
2024-11-22 20:12:36 +0500

    add event controllers

--- debugDivas/events/views.py
+++ debugDivas/events/views.py
@@ -1,18 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import viewsets
-from .models import User, SportType, Location
-from .serializers import UserSerializer, SportTypeSerializer, LocationSerializer
+from .models import Event
+from .serializers import EventSerializer
 
-class SportTypeViewSet(viewsets.ModelViewSet):
-    queryset = SportType.objects.all()
-    serializer_class = SportTypeSerializer
-
-class LocationViewSet(viewsets.ModelViewSet):
-    queryset = Location.objects.all()
-    serializer_class = LocationSerializer
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
